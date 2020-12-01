627ec8f894ba09fb252c09e44999aa88261b1572
Knut Schleßelmann <knut.schlesselmann@lagom.de>
2020-12-01 06:39:31 +0100

    test: use nested tests

--- 2020/src/test/kotlin/de/lagom/aoc2020/Day01Test.kt
+++ 2020/src/test/kotlin/de/lagom/aoc2020/Day01Test.kt
@@ -2,15 +2,16 @@ package de.lagom.aoc2020
 
 import mu.KotlinLogging
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 private val logger = KotlinLogging.logger {}
 
 internal class Day01Test {
 
-    @Test
-    fun `should solve part 1`() {
-        // Given
+    @Nested
+    inner class `Given test input` {
+
         val input = listOf(
             1721,
             979,
@@ -20,55 +21,56 @@ internal class Day01Test {
             1456,
         )
 
-        // When
-        val result = Day01.partOne(input)
+        @Nested
+        inner class `when solving part one` {
 
-        // Then
-        assertThat(result).isEqualTo(514579)
+            private val result = Day01.partOne(input)
+
+            @Test
+            fun `should yield correct result`() {
+                assertThat(result).isEqualTo(514579)
+            }
+        }
+
+        @Nested
+        inner class `when solving part two` {
+
+            private val result = Day01.partTwo(input)
+
+            @Test
+            fun `should yield correct result`() {
+                assertThat(result).isEqualTo(241861950)
+            }
+        }
     }
 
-    @Test
-    fun `should run part 1`() {
-        // Given
+    @Nested
+    inner class `Given actual input` {
+
         val input = Input.asList("day01.txt")
             .map { it.toInt() }
 
-        // When
-        val result = Day01.partOne(input)
+        @Nested
+        inner class `when solving part one` {
 
-        // Then
-        logger.info { "Result of Day 01, Part 01: $result" }
+            private val result = Day01.partOne(input)
+
+            @Test
+            fun `should log computed result`() {
+                logger.info { "Result of Day 01, Part 1: $result" }
+            }
+        }
+
+        @Nested
+        inner class `when solving part two` {
+
+            private val result = Day01.partTwo(input)
+
+            @Test
+            fun `should log computed result`() {
+                logger.info { "Result of Day 01, Part 2: $result" }
+            }
+        }
     }
 
-    @Test
-    fun `should solve part 2`() {
-        // Given
-        val input = listOf(
-            1721,
-            979,
-            366,
-            299,
-            675,
-            1456,
-        )
-
-        // When
-        val result = Day01.partTwo(input)
-
-        // Then
-        assertThat(result).isEqualTo(241861950)
-    }
-
-    @Test
-    fun `should run part 2`() {
-        // Given
-        val input = Input.asList("day01.txt")
-            .map { it.toInt() }
-
-        // When
-        val result = Day01.partTwo(input)
-
-        // Then
-        logger.info { "Result of Day 01, Part 02: $result" }
-    }
 }
