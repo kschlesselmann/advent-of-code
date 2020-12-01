6b24eb09d1de54b766cb010fa3c2e36dab630c3b
Knut Schleßelmann <knut.schlesselmann@lagom.de>
2020-12-01 06:24:42 +0100

    feat: day 1, part 2

--- 2020/src/main/kotlin/de/lagom/aoc2020/Day01.kt
+++ 2020/src/main/kotlin/de/lagom/aoc2020/Day01.kt
@@ -3,8 +3,18 @@ package de.lagom.aoc2020
 object Day01 {
 
     fun partOne(input: List<Int>) = input.asSequence()
-            .flatMap { a -> input.map { b -> a to b } }
-            .first { (a, b) -> a + b == 2020 }
-            .let { (a, b) -> a * b }
+        .flatMap { a -> input.map { b -> a to b } }
+        .first { (a, b) -> a + b == 2020 }
+        .let { (a, b) -> a * b }
+
+    fun partTwo(input: List<Int>) = input.asSequence()
+        .flatMap { a -> input.map { b -> a to b } }
+        .flatMap { (a, b) -> input.map { c -> Triple(a, b, c) } }
+        .first { it.sum() == 2020 }
+        .product()
+
+    private fun Triple<Int, Int, Int>.sum(): Int = first + second + third
+
+    private fun Triple<Int, Int, Int>.product(): Int = first * second * third
 
 }
--- 2020/src/test/kotlin/de/lagom/aoc2020/Day01Test.kt
+++ 2020/src/test/kotlin/de/lagom/aoc2020/Day01Test.kt
@@ -1,28 +1,74 @@
 package de.lagom.aoc2020
 
+import mu.KotlinLogging
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+private val logger = KotlinLogging.logger {}
+
 internal class Day01Test {
 
-        @Test
-        fun `should solve part 1`() {
-            // Given
-            val input = listOf(
-                1721,
-                979,
-                366,
-                299,
-                675,
-                1456,
-            )
+    @Test
+    fun `should solve part 1`() {
+        // Given
+        val input = listOf(
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456,
+        )
 
-            // When
-            val result = Day01.partOne(input)
+        // When
+        val result = Day01.partOne(input)
 
-            // Then
-            assertThat(result).isEqualTo(514579)
-        }
+        // Then
+        assertThat(result).isEqualTo(514579)
+    }
 
+    @Test
+    fun `should run part 1`() {
+        // Given
+        val input = Input.asList("day01.txt")
+            .map { it.toInt() }
 
+        // When
+        val result = Day01.partOne(input)
+
+        // Then
+        logger.info { "Result of Day 01, Part 01: $result" }
+    }
+
+    @Test
+    fun `should solve part 2`() {
+        // Given
+        val input = listOf(
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456,
+        )
+
+        // When
+        val result = Day01.partTwo(input)
+
+        // Then
+        assertThat(result).isEqualTo(241861950)
+    }
+
+    @Test
+    fun `should run part 2`() {
+        // Given
+        val input = Input.asList("day01.txt")
+            .map { it.toInt() }
+
+        // When
+        val result = Day01.partTwo(input)
+
+        // Then
+        logger.info { "Result of Day 01, Part 02: $result" }
+    }
 }
