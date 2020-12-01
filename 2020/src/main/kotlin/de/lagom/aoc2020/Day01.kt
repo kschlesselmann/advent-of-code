8e2b251038de1c8c8df9c05a8bf2a81cb2213673
Knut Schleßelmann <knut.schlesselmann@lagom.de>
2020-12-01 07:00:18 +0100

    refactor: add some meaningful extensions

--- 2020/src/main/kotlin/de/lagom/aoc2020/Day01.kt
+++ 2020/src/main/kotlin/de/lagom/aoc2020/Day01.kt
@@ -2,16 +2,36 @@ package de.lagom.aoc2020
 
 object Day01 {
 
-    fun partOne(input: List<Int>) = input.asSequence()
-        .flatMap { a -> input.map { b -> a to b } }
-        .first { (a, b) -> a + b == 2020 }
-        .let { (a, b) -> a * b }
-
-    fun partTwo(input: List<Int>) = input.asSequence()
-        .flatMap { a -> input.map { b -> a to b } }
-        .flatMap { (a, b) -> input.map { c -> Triple(a, b, c) } }
+    fun partOne(input: List<Int>) = input.toAllTuples()
         .first { it.sum() == 2020 }
         .product()
+
+    fun partTwo(input: List<Int>) = input.toAllTriples()
+        .first { it.sum() == 2020 }
+        .product()
+
+    private fun List<Int>.toAllTuples(): Sequence<Pair<Int, Int>> {
+        val input = this
+
+        return sequence {
+            input.forEach { x ->
+                input.forEach { y ->
+                    yield(Pair(x, y))
+                }
+            }
+        }
+    }
+
+    private fun Pair<Int, Int>.sum(): Int = first + second
+
+    private fun Pair<Int, Int>.product(): Int = first * second
+
+    private fun List<Int>.toAllTriples(): Sequence<Triple<Int, Int, Int>> {
+        val input = this
+
+        return input.toAllTuples()
+            .flatMap { (x, y) -> input.map { z -> Triple(x, y, z) } }
+    }
 
     private fun Triple<Int, Int, Int>.sum(): Int = first + second + third
 
